780b2937e6165f3d11556cd75384c893e3629444
sowmya30062007 <sowmyapanagam007@gmail.com>
2025-11-25 11:52:56 +0530

    Add files via upload

---
+++ Bitwise operators.c output.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+	flote celsius,fahrenheit;
+	printf("Enter Temp in Fahrenheit:");
+	scanf("%f",&fahrenheit);
+	celsius=(Fahrenheit-32)/1.8;
+	printf("Temperature in Celcius:%.2f",celcius);
+	 return 0;
+}
