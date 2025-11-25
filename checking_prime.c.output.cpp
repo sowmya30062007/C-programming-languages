744084340ead1cfbf059a6764039fb95001fd515
sowmya30062007 <sowmyapanagam007@gmail.com>
2025-11-25 11:55:11 +0530

    Add files via upload

---
+++ checking_prime.c.output.cpp
@@ -0,0 +1,17 @@
+//given number is a prime or not
+#include <stdio.h>
+int main ()
+{
+	int n,i,factor_count=0;
+	printf("enter a number:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+    {
+     	if (n%i==0)factor_count++;
+	}
+	//primes contain only 2 factors (1 and itself)
+	if (factor_count==2)
+	printf("%d is a prime",n);
+	else printf("%d is not a prime",n);
+	return 0;
+} 
